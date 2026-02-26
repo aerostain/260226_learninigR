c2d7fbdf25cfba3b4a0e49984dbf5cd4bdc3a512
r1ck7 <richims026@gmail.com>
2026-02-26 05:51:10 -0500

    'update_1'

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Léeme
+Desde hoy debo confiar más y meojorar de a poco, sí es posible!.
+
+> No olvides escribir alegremente, solo tu puedes hacerlo!.
+
+## Día 26/02/26
+
+Objetivos de hoy:
+
+1. Análisis univariado de datos. Debo resumir las estadísticas principales de cualquier vector de datos continuos o no.
+    * Generar un función que pueda calcular las estádisticas necesarias.
+    * Usarla con dplyr de forma profesional (ungroup y lo demás etc.) usando librería broom.
+    * Intervalos de confianza y pruebas de hipotesis para continuas, ordinales y proporciones.
+    * Gráficar con ggplot.
+
+2. Crear un compilado de información llamado: "Mis avances en conocimiento estadístico" con su propio github y usar formato APA (para practicar y porque es útil), formato html.
+
+> Recuerda ir de a pocos y no ser muy exigente, todavía estas aprendiendo. No desesperes que la marea puede cambiar en el minuto menos esperado y solo los experimentados saben como ganarle al mar porque ya lo conocieron muchas veces antes.
+
---
+++ main.R
@@ -0,0 +1,50 @@
+# Librerías
+library(tidyverse)
+library(magrittr)
+library(ggplot2)
+library(gridExtra)
+library(expss)
+library(ggdark)
+library(lubridate)
+library(DBI)
+library(RMySQL)
+library(sf)
+library(terra)
+library(haven)
+library(rio)
+library(broom)
+library(pacman)
+library(shiny)
+library(devtools)
+
+# .lintr
+text <- c(
+  "linters: linters_with_defaults(",
+  "  assignment_linter(allow_pipe_assign = TRUE),",
+  "  object_usage_linter = NULL",
+  "  )"
+)
+pathfile <- file.path(getwd(), ".lintr")
+writeLines(text, pathfile)
+
+# Git
+system("git init")
+system("git remote add repos
+https://github.com/aerostain/260226_learninigR.git")
+system("git remote -v")
+
+system("git status")
+system("git add .")
+system("git status")
+system("git commit -m 'Init'")
+system("git log")
+system("git push repos master")
+
+dir.create("Databases")
+dir.create("Documents")
+file.create("Readme.md")
+unlink("Informe.qmd", recursive = TRUE)
+
+# ---------------------------------------------------------------------
+#
+# ---------------------------------------------------------------------
